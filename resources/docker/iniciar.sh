47595642625c7ad07daceb5cd9d02a559f912df5
francoprieto <francoprieto@gmail.com>
2026-01-14 11:23:17 -0300

    Se construye una imagen de python que incluye curl y tar

--- resources/docker/iniciar.sh
+++ resources/docker/iniciar.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+cd ./jenkins
 # Construimos la imagen para Jenkins
 docker build -t myjenkins-blueocean:2.516.3-1 .
+
+cd ../python
+docker build -t my-python:1.0.0 .
+
+cd ..
 
 # Obtenemos el GID del grupo docker (necesario para compartir el docker.sock)
 export DOCKER_GID=$(getent group docker | awk -F: '{print $3}')
